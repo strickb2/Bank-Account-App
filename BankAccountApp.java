5b022ab842dbfb0d5be99114a02885fec975d549
strickb2 <63793076+strickb2@users.noreply.github.com>
2020-10-02 14:58:39 +0100

    Add files via upload

---
+++ BankAccountApp.java
@@ -0,0 +1,37 @@
+package bankaccountapp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BankAccountApp {
+
+	public static void main(String[] args) {
+		List<Account> accounts = new LinkedList<Account>();
+		
+		
+		// Read a CSV file then create new accounts based on that data
+		String file = "C:\\Users\\ben99\\eclipse-workspace\\Project1\\NewBankAccounts.csv";
+		List<String[]> newAccountHolders = utilities.CSV.read(file);
+		for (String[] accountHolder : newAccountHolders) {
+			String name = accountHolder[0];
+			String sSN = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+			if (accountType.equals("Savings")) {
+				accounts.add(new Savings(name, sSN, initDeposit));
+			}
+			else if (accountType.equals("Checking")) {
+				accounts.add(new Checking(name, sSN, initDeposit));
+			}
+			else {
+				System.out.println("Error Reading Account Type");
+			}
+		}
+		
+		for (Account acc : accounts) {
+			System.out.println("\n**********");
+			acc.showInfo();
+		}
+	}
+
+}
